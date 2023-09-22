356d5e03a1f0574ee0ed59f511f7e7a77e778eda
Ajan Eshwara <ajan.eshwara@gmail.com>
2023-09-22 21:56:40 +0530

    Demo2 commit

--- Demo1.java
+++ Demo1.java
@@ -1,5 +1,31 @@
+import java.util.Scanner;
+
 public class Demo1 {
+    private static final Scanner scanner = new Scanner(System.in);
     public static void main(String[] args) {
-        
+        outer:
+        while(true){ 
+         
+         System.out.print("Enter your NIC: ");
+         String nic = scanner.nextLine();
+ 
+         if(nic.length() !=10){
+             System.out.println("invalid nic");
+             continue;
+         }
+ 
+         for(int i =0; i<9; i++){
+             if(!Character.isDigit(nic.charAt(i))){
+                 System.out.println("Inavalid");
+                 continue outer;
+             }
+         }
+ 
+         if(!(nic.endsWith("v") || nic.endsWith("V"))){
+             System.out.println("Invalid");
+            continue;
+         }
+         System.out.println("valid NIC");
+         }
     }
 }
